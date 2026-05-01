0f724460c49c2ef1ceb88f643b53a7e644dc33b8
Gabriel Silva <dev.gabriellucas@gmail.com>
2026-05-01 10:48:22 -0300

    feat: filtro por categoria em vendas

--- frontend/comandaDigital/src/pages/vendas/Vendas.jsx
+++ frontend/comandaDigital/src/pages/vendas/Vendas.jsx
@@ -1,34 +1,32 @@
 import { useEffect, useState } from "react";
 import Sidebar from "../../components/sidebar/Sidebar";
 import "./Vendas.css";
-import axios from 'axios' 
+import axios from "axios";
 import Cart from "../../components/cart/Cart";
 import ModalComplementos from "../../components/modalComplementos/ModalComplementos";
 
 function Vendas() {
-
-  const [categorias, setCategorias] = useState([])
-  const [produtos, setProdutos] = useState([])
+  const [categorias, setCategorias] = useState([]);
+  const [produtos, setProdutos] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:3031/categorias')
-    .then(res => {
-      // console.log(res.data)
-      setCategorias(res.data)
-    })
+    axios.get("http://localhost:3031/categorias").then((res) => {
+      setCategorias(res.data);
+    });
 
-    axios.get('http://localhost:3031/produtos')
-    .then(res => setProdutos(res.data))
-    .catch(error => console.log(error))
-  }, [])
+    axios
+      .get("http://localhost:3031/produtos")
+      .then((res) => setProdutos(res.data))
+      .catch((error) => console.log(error));
+  }, []);
 
-  const [cart, setCart] = useState([])
-  function addToCart(produto, complementosSel){
+  const [cart, setCart] = useState([]);
+  function addToCart(produto, complementosSel) {
     const novoItem = {
       ...produto,
-      complementos: complementosSel
-    }
-    setCart(prev => [...prev, novoItem])
+      complementos: complementosSel,
+    };
+    setCart((prev) => [...prev, novoItem]);
   }
 
   //==========================
@@ -36,50 +34,54 @@ function Vendas() {
   //==========================
 
   //mais para frente tentar fazer isso dinamicamente, adicionando uma regra de negócio para isso
-  const nessecitaComplemento = "Açaí"
+  const nessecitaComplemento = "Açaí";
   const verificaProduto = (produto) => {
-    return nessecitaComplemento.includes(produto.nome_categoria)
-  } 
+    return nessecitaComplemento.includes(produto.nome_categoria);
+  };
 
   //==========================
   // MODAL
   //==========================
 
-  const [isOpen, setIsOpen] = useState(false)
-  const closeModal = () => setIsOpen(false)
+  const [isOpen, setIsOpen] = useState(false);
+  const closeModal = () => setIsOpen(false);
 
   //==========================
   // BUTTON CARRINHO HANDLER
   //==========================
 
-  const [produtoSelecionado, setProdutoSelecionado] = useState()
+  const [produtoSelecionado, setProdutoSelecionado] = useState();
 
   function handleAddProduto(produto) {
-    try{
-      if(verificaProduto(produto)){
-        setProdutoSelecionado(produto)
-        setIsOpen(true)
-      }else{
-        setProdutoSelecionado(produto)
-        addToCart(produto, [])
+    try {
+      if (verificaProduto(produto)) {
+        setProdutoSelecionado(produto);
+        setIsOpen(true);
+      } else {
+        setProdutoSelecionado(produto);
+        addToCart(produto, []);
       }
-    }catch(error){
-      console.log(error)
+    } catch (error) {
+      console.log(error);
     }
   }
 
   //==========================
-  // BUTTON REMOVER DO CARRINHO 
+  // BUTTON REMOVER DO CARRINHO
   //==========================
 
-  function removeProd(index){
+  function removeProd(index) {
     // const produto = setCart(prev => prev.id_produto === prod.id_produto)
-    setCart(prev => prev.filter((_, i) => i !== index))
+    setCart((prev) => prev.filter((_, i) => i !== index));
   }
 
   //==========================
-  // HTML
-  //==========================  
+  // SHOW CATEGORIA
+  //==========================
+
+  const [categoriaAtiva, setCategoriaAtiva] = useState(null);
+  const produtosFiltrados = categoriaAtiva ? produtos.filter(p => p.nome_categoria === categoriaAtiva) : produtos
+
   return (
     <>
       <section className="container-vendas">
@@ -101,11 +103,16 @@ function Vendas() {
             <div className="sidebar-itens">
               <div className="itens-list">
                 <ul>
-                  <li>Todos</li>
+                  <li>
+                    <button className="list-button" onClick={() => setCategoriaAtiva(null)}>Todos</button>
+                  </li>
                   {categorias.map((cat, i) => {
-                    return(
-                      <li key={i}>{cat.nome_categoria}</li>
-                    )
+                    return (
+                      <li key={i}>
+                        {/* fazer onClick para setar a categoria como o nome da clicada e fazer uma logica pra mostrar apenas o itens com a categoria que tem o nome */}
+                        <button onClick={() => setCategoriaAtiva(cat.nome_categoria)}>{cat.nome_categoria}</button>
+                      </li>
+                    );
                   })}
                 </ul>
               </div>
@@ -113,43 +120,41 @@ function Vendas() {
 
             <div className="vendas-content">
               <div className="vendas-layout">
-
-                {produtos.map((prod, i) => {
-                  return(
-                <div className="card-venda" key={i}>
-                  <p>{prod.nome_produto}</p>
-                  <h4>R${prod.preco_produto}</h4>
-                  <button className="button-padrao2" onClick={() => handleAddProduto(prod)} >Adicionar ao carrinho</button>
-                </div>
-                  )
+                {produtosFiltrados.map((prod, i) => {
+                  return (
+                    <div className="card-venda" key={i}>
+                      <p>{prod.nome_produto}</p>
+                      <h4>R${prod.preco_produto}</h4>
+                      <button
+                        className="button-padrao2"
+                        onClick={() => handleAddProduto(prod)}
+                      >
+                        Adicionar ao carrinho
+                      </button>
+                    </div>
+                  );
                 })}
-
               </div>
             </div>
 
             <div className="modal-complementos">
-              {isOpen 
-              ?
-              < ModalComplementos 
-              onClose={closeModal}
-              onConfirm={(selecionados) => {
-                addToCart(produtoSelecionado, selecionados)
-                setIsOpen(false)
-              }}
-              /> 
-              : 
-              ""
-              }
+              {isOpen ? (
+                <ModalComplementos
+                  onClose={closeModal}
+                  onConfirm={(selecionados) => {
+                    addToCart(produtoSelecionado, selecionados);
+                    setIsOpen(false);
+                  }}
+                />
+              ) : (
+                ""
+              )}
             </div>
-
           </div>
         </div>
 
         <div className="cart">
-          <Cart 
-           cart={cart} 
-           onRemove={removeProd} 
-          />
+          <Cart cart={cart} onRemove={removeProd} />
         </div>
       </section>
     </>
